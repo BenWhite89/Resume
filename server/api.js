a13ac5f3e181a5c0483fe2fbf43d7d817209a4bc
Benjamin White <32458896+BenWhite89@users.noreply.github.com>
2018-01-14 23:06:03 -0600

    Add js to heroku build

---
+++ server/api.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express = require("express");
+let router = express();
+// router.use('/experience');
+// router.use('/skills');
+// router.use('/projects');
+exports.default = router;
---
+++ server/index.js
@@ -0,0 +1,16 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const assetcheck_mw_1 = require("./middleware/assetcheck.mw");
+const api_1 = require("./api");
+let app = express();
+let clientPath = path.join(__dirname, '../client');
+app.use(express.static(clientPath));
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.get('*', assetcheck_mw_1.default);
+app.use('/api', api_1.default);
+app.listen(process.env.PORT || 3000);

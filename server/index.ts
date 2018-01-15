0474e23de4d971fd43c0b2b01c40fa24e5e01e32
Benjamin White <32458896+BenWhite89@users.noreply.github.com>
2018-01-14 22:55:33 -0600

    Heroku launch error fix

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,7 @@
 import * as express from 'express';
 import * as bodyParser from 'body-parser';
 import * as path from 'path';
+import * as cookieParser from 'cookie-parser';
 import check from './middleware/assetcheck.mw';
 import api from './api';
 
@@ -10,6 +11,7 @@ let clientPath = path.join(__dirname, '../client');
 
 app.use(express.static(clientPath));
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 app.get('*', check);
 app.use('/api', api);
